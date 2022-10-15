7474955055bd23f786219e94fdbe2429b3a8aaf6
Gabriel Brancom <gbanco1993@gmail.com>
2022-10-15 03:58:33 -0300

    finalizado sessões tecnologia e projetos

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
+const labels = [
+  'HTML',
+  'JS',
+  'React',
+  'CSS',
+  'Node',
+  'Redux',
+  'TS',
+];
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Skills',
+    backgroundColor: [
+      'rgb(255, 136, 0)',
+      'rgb(255, 255, 0)',
+      'rgb(0, 253, 55)',
+      'rgb(165, 42, 42)',
+      'rgb(128, 128, 128)',
+      'rgb(127, 255, 212)',
+      'rgb(104, 57, 93)',
+    ],
+    data: [100, 100, 90, 100, 50, 80, 30],
+  }]
+};
 
-typeWriter();
+const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    indexAxis: 'y',
+    responsive: true,
+  },
+};
+
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
