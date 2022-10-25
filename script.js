634a9d5ecc03cd543bbb454ba78bf23b5e3f1694
Gabriel Branco <gbanco1993@gmail.com>
2022-10-25 20:46:17 -0300

    finalizado layout desktop

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
-const labels = [
-  'HTML',
-  'JS',
-  'React',
-  'CSS',
-  'Node',
-  'Redux',
-  'TS',
-];
-
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'Skills',
-    backgroundColor: [
-      'rgb(173, 255, 47)',
-    ],
-    data: [100, 100, 90, 100, 50, 80, 30],
-  }]
-};
-
-const config = {
-  type: 'bar',
-  data: data,
-  options: {
-    indexAxis: 'y',
-    responsive: true,
-  },
-};
-
-const myChart = new Chart(
-  document.getElementById('myChart'),
-  config
-);
-
 const lazyLoad = () => {
   const lazyClass = document.querySelectorAll('.lazy_load');
   return lazyClass.forEach((item) => {
     if (item.getBoundingClientRect().top < window.innerHeight) {
       item.style.opacity = 1;
       item.style.transform = "translateX(0)";
-    }
+    } 
   });
 }
 window.addEventListener('scroll', lazyLoad);
